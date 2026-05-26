22abc1bca1bcc7806c7259ed14ceff0389a7e241
Thetyunn <thetyoon526@gmail.com>
2026-05-26 19:37:22 +0630

    update

--- assigns.js
+++ assigns.js
@@ -31,3 +31,32 @@ for (let d = 1; d <= 3; d++) {
   }
   console.log(chr);
 }
+
+console.log("Assignment-5");
+for (let i = 1; i <= 7; i++) {
+  switch (i) {
+    case 1:
+      console.log("Day 1: Monday");
+      break;
+    case 2:
+      console.log("Day 2: Tuesday");
+      break;
+    case 3:
+      console.log("Day 3: Wednesday");
+      break;
+    case 4:
+      console.log("Day 4: Thursday");
+      break;
+    case 5:
+      console.log("Day 5: Friday");
+      break;
+    case 6:
+      console.log("Day 6: Saturday");
+      break;
+    case 7:
+      console.log("Day 7: Sunday");
+      break;
+    default:
+      console.log("Invalid day");
+  }
+}
--- fuel-effi.js
+++ fuel-effi.js
@@ -1,6 +1,6 @@
 let trip = { distance: 300, effcienty: 25, cost: 3.75 };
 
-let require = trip.distance / trip.effcienty;
-let Trip_cost = require * trip.cost;
-console.log(`Fuel needed : ${require} gallon`);
+let Fuel_needed = trip.distance / trip.effcienty;
+let Trip_cost = Fuel_needed * trip.cost;
+console.log(`Fuel needed : ${Fuel_needed} gallon`);
 console.log(`The total cost of Trip : $${Trip_cost}`);
